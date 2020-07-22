2a18c04f57a7f3faf25b2ae0841940bbf224315d
TH1NKFASTER <46980911+TH1NKFASTER@users.noreply.github.com>
2020-07-22 19:46:14 +0300

    Create create_abel_laureates.sql

---
+++ create_abel_laureates.sql
@@ -0,0 +1,65 @@
+CREATE TABLE abel_laureates
+    (
+        laureate_name varchar,
+        country_of_citizenship varchar,
+        date_of_death varchar,
+        educated_at varchar,
+        day_of_birth BIGINT,
+        month_of_birth BIGINT,
+        year_of_birth BIGINT
+    );
+
+INSERT INTO public.abel_laureates (laureate_name, country_of_citizenship, date_of_death, educated_at, day_of_birth,
+                                   month_of_birth, year_of_birth)
+VALUES ('Peter_Lax', 'Hungary', NULL, 'New_York_University', 1, 5, 1926);
+INSERT INTO public.abel_laureates (laureate_name, country_of_citizenship, date_of_death, educated_at, day_of_birth,
+                                   month_of_birth, year_of_birth)
+VALUES ('Pierre_Deligne', 'Belgium', NULL, 'Université_libre_de_Bruxelles', 3, 10, 1944);
+INSERT INTO public.abel_laureates (laureate_name, country_of_citizenship, date_of_death, educated_at, day_of_birth,
+                                   month_of_birth, year_of_birth)
+VALUES ('Endre_Szemerédi', 'Hungary', NULL, 'Eötvös_Loránd_University', 21, 8, 1940);
+INSERT INTO public.abel_laureates (laureate_name, country_of_citizenship, date_of_death, educated_at, day_of_birth,
+                                   month_of_birth, year_of_birth)
+VALUES ('John_Forbes_Nash_Jr.', 'United_States_of_America', '23 May 2015', 'Princeton_Universityy', 13, 6, 1928);
+INSERT INTO public.abel_laureates (laureate_name, country_of_citizenship, date_of_death, educated_at, day_of_birth,
+                                   month_of_birth, year_of_birth)
+VALUES ('Michael_Atiyah', 'United_Kingdom', NULL, 'Trinity_College', 22, 4, 1929);
+INSERT INTO public.abel_laureates (laureate_name, country_of_citizenship, date_of_death, educated_at, day_of_birth,
+                                   month_of_birth, year_of_birth)
+VALUES ('Jacques_Tits', 'France', NULL, 'Université_libre_de_Bruxelles', 12, 8, 1930);
+INSERT INTO public.abel_laureates (laureate_name, country_of_citizenship, date_of_death, educated_at, day_of_birth,
+                                   month_of_birth, year_of_birth)
+VALUES ('Jean-Pierre_Serre', 'France', NULL, 'University_of_Paris', 15, 9, 1926);
+INSERT INTO public.abel_laureates (laureate_name, country_of_citizenship, date_of_death, educated_at, day_of_birth,
+                                   month_of_birth, year_of_birth)
+VALUES ('Mikhail_Leonidovich_Gromov', 'Soviet_Union', NULL, 'Mathematics_and_Mechanics_Faculty', 23, 12, 1943);
+INSERT INTO public.abel_laureates (laureate_name, country_of_citizenship, date_of_death, educated_at, day_of_birth,
+                                   month_of_birth, year_of_birth)
+VALUES ('Isadore_Singer', 'United_States_of_America', NULL, 'University_of_Michigan', 24, 4, 1924);
+INSERT INTO public.abel_laureates (laureate_name, country_of_citizenship, date_of_death, educated_at, day_of_birth,
+                                   month_of_birth, year_of_birth)
+VALUES ('John_Milnor', 'United_States_of_America', NULL, 'Princeton_University', 20, 2, 1931);
+INSERT INTO public.abel_laureates (laureate_name, country_of_citizenship, date_of_death, educated_at, day_of_birth,
+                                   month_of_birth, year_of_birth)
+VALUES ('S._R._Srinivasa_Varadhan', 'India', NULL, 'Indian_Statistical_Institute', 2, 1, 1940);
+INSERT INTO public.abel_laureates (laureate_name, country_of_citizenship, date_of_death, educated_at, day_of_birth,
+                                   month_of_birth, year_of_birth)
+VALUES ('Yakov_Sinai', 'Russia', NULL, 'Moscow_State_University', 21, 9, 1935);
+INSERT INTO public.abel_laureates (laureate_name, country_of_citizenship, date_of_death, educated_at, day_of_birth,
+                                   month_of_birth, year_of_birth)
+VALUES ('Andrew_Wiles', 'United_Kingdom', NULL, 'Clare_College', 11, 4, 1953);
+INSERT INTO public.abel_laureates (laureate_name, country_of_citizenship, date_of_death, educated_at, day_of_birth,
+                                   month_of_birth, year_of_birth)
+VALUES ('John_Tate', 'United_States_of_America', NULL, 'Princeton_University', 13, 3, 1925);
+INSERT INTO public.abel_laureates (laureate_name, country_of_citizenship, date_of_death, educated_at, day_of_birth,
+                                   month_of_birth, year_of_birth)
+VALUES ('Lennart_Carleson', 'Sweden', NULL, 'Uppsala_University', 18, 3, 1928);
+INSERT INTO public.abel_laureates (laureate_name, country_of_citizenship, date_of_death, educated_at, day_of_birth,
+                                   month_of_birth, year_of_birth)
+VALUES ('Louis_Nirenberg', 'Canada', NULL, 'McGill_University', 28, 2, 1925);
+INSERT INTO public.abel_laureates (laureate_name, country_of_citizenship, date_of_death, educated_at, day_of_birth,
+                                   month_of_birth, year_of_birth)
+VALUES ('Yves_Meyer', 'France', NULL, 'École_Normale_Supérieure', 19, 7, 1939);
+INSERT INTO public.abel_laureates (laureate_name, country_of_citizenship, date_of_death, educated_at, day_of_birth,
+                                   month_of_birth, year_of_birth)
+VALUES ('John_G._Thompson', 'United_States_of_America', NULL, 'Yale_University', 13, 10, 1932);
